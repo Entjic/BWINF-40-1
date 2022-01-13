f756ee0a1bad43926cf85b78adcb3a6f6e115a18
Entjic <entjic@gmail.com>
2022-01-13 08:18:29 +0100

    Delete Wuerfelglueck directory

--- Wuerfelglueck/src/main/java/de/entjic/Main.java
+++
@@ -1,9 +0,0 @@
-package de.entjic;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
--- Wuerfelglueck/src/main/java/de/entjic/game/Team.java
+++
@@ -1,9 +0,0 @@
-package de.entjic.game;
-
-public enum Team {
-    EMPTY,
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW;
-}
